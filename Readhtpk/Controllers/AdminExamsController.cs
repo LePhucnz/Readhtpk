fe2a82862fb0f8ad52d59b504eaa455fee805157
lee phucs <lhmai0910@gmail.com>
2026-03-23 16:08:25 +0700

    feat: Them Admin CRUD cho Subject, Question, Exam

---
+++ Readhtpk/Controllers/AdminExamsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Readhtpk.Data;
+using Readhtpk.Models;
+
+namespace Readhtpk.Controllers
+{
+    public class AdminExamsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminExamsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AdminExams
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Exams.Include(e => e.Subject);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: AdminExams/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exam = await _context.Exams
+                .Include(e => e.Subject)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+
+            return View(exam);
+        }
+
+        // GET: AdminExams/Create
+        public IActionResult Create()
+        {
+            ViewBag.SubjectId = new SelectList(_context.Subjects, "Id", "Name");
+            return View();
+        }
+
+        // POST: AdminExams/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,DurationMinutes,TotalQuestions,TotalMarks,ExamDate,IsActive,SubjectId,CreatedBy")] Exam exam)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(exam);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.SubjectId = new SelectList(_context.Subjects, "Id", "Name", exam.SubjectId);
+            return View(exam);
+        }
+
+        // GET: AdminExams/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exam = await _context.Exams.FindAsync(id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+            ViewBag.SubjectId = new SelectList(_context.Subjects, "Id", "Name", exam.SubjectId);
+            return View(exam);
+        }
+
+        // POST: AdminExams/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,DurationMinutes,TotalQuestions,TotalMarks,ExamDate,IsActive,SubjectId,CreatedBy")] Exam exam)
+        {
+            if (id != exam.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(exam);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExamExists(exam.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.SubjectId = new SelectList(_context.Subjects, "Id", "Name", exam.SubjectId);
+            return View(exam);
+        }
+
+        // GET: AdminExams/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exam = await _context.Exams
+                .Include(e => e.Subject)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+
+            return View(exam);
+        }
+
+        // POST: AdminExams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var exam = await _context.Exams.FindAsync(id);
+            if (exam != null)
+            {
+                _context.Exams.Remove(exam);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExamExists(int id)
+        {
+            return _context.Exams.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Readhtpk/Controllers/AdminSubjectsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Readhtpk.Data; // Kiểm tra namespace DbContext của bạn
+using Readhtpk.Models; // Kiểm tra namespace Model của bạn
+
+namespace Readhtpk.Controllers
+{
+    public class AdminSubjectsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminSubjectsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AdminSubjects
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Subjects.ToListAsync());
+        }
+
+        // GET: AdminSubjects/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+
+        // GET: AdminSubjects/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminSubjects/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Code,Description,IsActive")] Subject subject)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(subject);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(subject);
+        }
+
+        // GET: AdminSubjects/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects.FindAsync(id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+            return View(subject);
+        }
+
+        // POST: AdminSubjects/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Code,Description,IsActive")] Subject subject)
+        {
+            if (id != subject.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(subject);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SubjectExists(subject.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(subject);
+        }
+
+        // GET: AdminSubjects/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+
+        // POST: AdminSubjects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var subject = await _context.Subjects.FindAsync(id);
+            if (subject != null)
+            {
+                _context.Subjects.Remove(subject);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SubjectExists(int id)
+        {
+            return _context.Subjects.Any(e => e.Id == id);
+        }
+    }
+}
